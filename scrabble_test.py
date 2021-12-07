850860ff7c4ac92ba04de090ac3790c83be54a83
HugeCoderGuy <alexscottlewis@gmail.com>
2021-12-06 16:47:11 -0800

    "12/6/2021 challenge leve 5. Completed my final assigment in college."

---
+++ scrabble_test.py
@@ -0,0 +1,24 @@
+"""
+Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+
+Notes:
+
+Only lower case letters will be used (a-z). No punctuation or digits will be included.
+Performance needs to be considered
+Input strings s1 and s2 are null terminated.
+Examples
+
+scramble('rkqodlw', 'world') ==> True
+scramble('cedewaraaossoqqyt', 'codewars') ==> True
+scramble('katas', 'steak') ==> False
+"""
+from itertools import permutations
+
+
+def scramble(s1, s2):
+    perms = [''.join(p) for p in permutations(s1)]
+    if s2 in perms:
+        return True
+
+
+
