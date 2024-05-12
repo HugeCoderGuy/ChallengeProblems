95746cf0166af6e961523ee39ec7c53197a458e0
Alex Lewis <81666253+HugeCoderGuy@users.noreply.github.com>
2024-05-12 09:38:12 -0700

    5/12/24 suboptimal then replaced with optimal

---
+++ length_last_word_int150.cpp
@@ -0,0 +1,76 @@
+/*
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal 
+substring
+ consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only English letters and spaces ' '.
+There will be at least one word in s.
+
+*/
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        std::reverse(s.begin(), s.end());
+        int counter = 0;
+        bool count_next_space = false;
+        for (char c : s) {
+            if ((c == ' ') & count_next_space) {
+                break;
+            }
+            else {
+                if (c != ' ') {
+                    counter++;
+                    count_next_space = true;
+                }
+            }
+        }
+        return counter;
+        // int counter = 0;
+        // int last_count = 0;
+        // bool last_char_space = false;
+        // for (char c : s) {
+        //     if (c == ' ') {
+        //         if (counter != 0) {
+        //             last_count = counter;
+        //         }
+        //         counter = 0;
+        //         last_char_space = true;
+        //     }
+        //     else {
+        //         counter++;
+        //         last_char_space = false;
+        //     }
+        // }
+        // if (last_char_space) {
+        //     return last_count;
+        // }
+        // else {
+        //     return counter;
+        // }
+    }
+};
