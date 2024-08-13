d2c0d87b44ef6f248db4f0f5c84c50090cc5dbf9
Alex Lewis <81666253+HugeCoderGuy@users.noreply.github.com>
2024-08-13 14:27:22 -0700

    fixed bug w/ head of list assignment

--- merge_two_sorted_lists.c
+++ merge_two_sorted_lists.c
@@ -1,4 +1,4 @@
-/*
+/**
 You are given the heads of two sorted linked lists list1 and list2.
 
 Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
@@ -27,9 +27,7 @@ Constraints:
 The number of nodes in both lists is in the range [0, 50].
 -100 <= Node.val <= 100
 Both list1 and list2 are sorted in non-decreasing order.
-*/
 
-/**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -40,24 +38,13 @@ Both list1 and list2 are sorted in non-decreasing order.
 #include <stdlib.h>
 
 #define DEBUG 1
-int MIN_VAL = -101;
-
-struct ListNode {
-    int val;
-    struct ListNode *next;
-};
-
-#include <stdio.h>
-#include <stdlib.h>
-
-#define DEBUG 1
-int MIN_VAL = -101;
+int MAX_VAL = 101;
 
 
 struct ListNode* addToList(struct ListNode* list, int numb) {
     struct ListNode* next_node = (struct ListNode*)malloc(sizeof(struct ListNode));;
-    next_node->next = NULL;  // ERROR HERE
-    list->val = numb;
+    next_node->val = numb; 
+    next_node->next = NULL;  
     list->next = next_node;
     return next_node;
 }
@@ -78,99 +65,26 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
         return result;
     }
 
-    while ((list1->next != NULL) & (list2->next != NULL)) {
-        if (list1->next != NULL) {
-            comp1 = list1->val;
-        }
-        else {
-            comp1 = MIN_VAL;
-        }
-        if (list2->next != NULL) {
-            comp2 = list2->val;
-        }
-        else{
-            comp2 = MIN_VAL;
-        }
-        if ((comp1 > comp2) | ((comp1 != MIN_VAL) & (comp2 == MIN_VAL))) {
-            printf("Adding list 1 val %d\n", comp1);
+    // while ((list1->next != NULL) | (list2->next != NULL)) {
+    while (list1 != NULL || list2 != NULL) {
+        int comp1 = (list1 != NULL) ? list1->val : MAX_VAL;
+        int comp2 = (list2 != NULL) ? list2->val : MAX_VAL;
+
+        if ((comp1 < comp2) | ((comp1 != MAX_VAL) & (comp2 == MAX_VAL))) {
             head = addToList(head, comp1);
             list1 = list1->next;
         }
-        else if ((comp1 < comp2) | ((comp2 != MIN_VAL) & (comp1 == MIN_VAL))) {
-            printf("Adding list 2 val %d\n", comp2);
+        else if ((comp1 > comp2) | ((comp2 != MAX_VAL) & (comp1 == MAX_VAL))) {
             head = addToList(head, comp2);
             list2 = list2->next;
         }
         else {
             if (comp1 == comp2) {
-                printf("Caught a comp=comp. numb1 is %d. numb2 id %d\n", comp1, comp2);
                 head = addToList(head, comp1);
                 list1 = list1->next;
             }
         }
     }
+    result = result->next;  // skip the head
     return result;
 }
-
-void test(struct ListNode* list1, struct ListNode* list2) {
-    struct ListNode* next_node = list1;
-    printf("TESTING\n\tInput1: ");
-    while (next_node->next != NULL) {
-        printf("%d ", next_node->val);
-        next_node = next_node->next;
-    }
-    printf("\n\tInput 2: ");
-    next_node = list2;
-    while (next_node->next != NULL) {
-        printf("%d ", next_node->val);
-        next_node = next_node->next;
-    }
-
-    printf("\n\tOutput: ");
-    next_node = mergeTwoLists(list1, list2);
-    while (next_node->next != NULL) {
-        printf("%d ", next_node->val);
-        next_node = next_node->next;
-    }
-    
-}
-
-int main() {
-    struct ListNode* list1;
-    struct ListNode* list2;
-    struct ListNode* head;
-    int i = 0;
-
-    head = list1;
-    for (i=0; i<6; i++) {
-        head = addToList(head, i);
-    }
-    head = list2;
-    for (i=0; i<6; i++) {
-        head = addToList(head, i);
-    }
-    test(list1, list2);
-
-    return 0;
-}
-
-/*
-list1 =
-[1,2,4]
-list2 =
-[1,3,4]
-[1,1,2,3,4,4]
-
-
-list2 =
-[]
-Output
-[]
-
-list1 =
-[]
-list2 =
-[0]
-Output
-[0]
-*/
